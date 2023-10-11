42ae1d392f456b943ec8395015ce8a0e06b09110
Pavel Avdeev <avdeevpavel19@gmail.com>
2023-10-11 14:04:42 +0300

    get all files of the current user

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\UploadFileRequest;
+use App\Models\File;
 use App\Services\Api\FileService;
 use App\Traits\HttpResponse;
 use Illuminate\Http\JsonResponse;
+use Mockery\Exception;
 
 class FileController extends Controller
 {
@@ -32,6 +34,34 @@ class FileController extends Controller
 
             return $this->created($downloadedFile);
         } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+
+    public function getFilesByUser()
+    {
+        try {
+            $currentUserID = \Auth::id();
+            $userFiles     = File::where('user_id', '=', $currentUserID)->get();
+
+            $userFilesData = [];
+
+            foreach ($userFiles as $userFile) {
+                $userFilesData[] = [
+                    'id'        => $userFile->id,
+                    'user_id'   => $userFile->user_id,
+                    'folder_id' => $userFile->folder_id,
+                    'file'      => $userFile->file,
+                    'name'      => $userFile->name,
+                    'sizeMB'    => $userFile->sizeMB,
+                    'format'    => $userFile->format,
+                    'path'      => $userFile->path,
+                    'hash'      => $userFile->hash,
+                ];
+            }
+
+            return $this->success($userFilesData);
+        } catch (Exception $e) {
             return $this->error($e->getMessage());
         }
     }
