222979e29ea5659e30c5950075ae678ed0239870
Pavel Avdeev <avdeevpavel19@gmail.com>
2023-10-17 15:44:37 +0300

    add route /update-login

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:api', 'verified']], function () {
     Route::get('user-info', [UserController::class, 'getInfo']);
+    Route::put('update-login', [UserController::class, 'updateLogin']);
     Route::post('create-folder', [FolderController::class, 'store']);
     Route::post('upload-file', [FileController::class, 'store']);
     Route::get('my/files', [FileController::class, 'getFilesByUser']);
